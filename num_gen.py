d72bf576bb5c71cc1ce2af55668e3f283b37e617
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2023-07-18 12:45:45 +0200

    Add functionality to one iteration

--- num_gen.py
+++ num_gen.py
@@ -1,11 +1,33 @@
 import numpy as np
+import random
 
-start, stop = 1,0
+start, stop = 1, 0
 
+# Asks for start and end value
 while start >= stop:
     start = int(input("Start value of intervall: "))
     stop = int(input("End value of intervall: "))
-    if start >= stop:
-        print("You stupid bastard, you've got no brain \n")
 
-numbers = np.arange(start, stop+1)
+    if start >= stop: # If start value is greater than end value
+        print("End value has to be greater than start value \n")
+
+numbers = np.arange(start, stop + 1) # [start, stop]
+length = len(numbers)
+
+print(f"Numbers array: {numbers}")
+print(f"Current length: {length}")
+
+index = random.randint(0, length - 1) # Randomly generates index for numbers list
+chosen_num = numbers[index]
+
+
+print(f"Chosen number: {chosen_num}")
+
+numbers = np.delete(numbers, chosen_num) # Removes chosen number from numbers array 
+length = len(numbers) # New length
+
+print(f"Numbers array: {numbers}")
+print(f"Current length: {length}")
+
+
+#while length > 0:
