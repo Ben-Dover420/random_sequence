4b17c497006d75986cba7e6a8c59d509ec667a4c
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2023-07-20 09:39:04 +0200

    Create window with title using tkinter

--- gui.py
+++ gui.py
@@ -1,3 +1,12 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 from num_gen import number_generator
 
+class NumberGen:
+    def __init__(self, root):
+        root.title("Number Generator")
+
+
+root = Tk()
+NumberGen(root)
+root.mainloop()
