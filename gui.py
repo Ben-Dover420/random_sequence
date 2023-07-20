fb42747b715adf661d16d226dc0648c2040ae632
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2023-07-20 11:21:14 +0200

    Change window size again and fix icon issue by downloading .ico image instead of .png

--- gui.py
+++ gui.py
@@ -4,9 +4,11 @@ from num_gen import number_generator
 
 class NumberGen(tk.Tk):
     def __init__(self):
+        super().__init__()
+
         self.title("Number Generator")
-        self.geometry("520x300")
-        self.iconbitmap("dice_icon.png")
+        self.geometry("900x500")
+        self.iconbitmap("dice_icon.ico")
 
 if __name__ == "__main__":
     root = NumberGen()
