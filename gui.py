fee01a560da706118336187f6f95add0f9de656c
Ben-Dover420 <b3nd0v3r2020@gmail.com>
2023-07-21 14:25:15 +0200

    Add font 'Playfair Display' to gui

--- gui.py
+++ gui.py
@@ -1,24 +1,19 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
 from num_gen import number_generator
-
-font_family = "Georgia"
-h1 = 25
-h2 = 15
-p = 10
-bg_color = "#008080"
-
 
 class NumberGen(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Number Generator")
-        self.geometry("300x170")
-        self.resizable(False, False)
+        self.geometry("600x300")
         self.iconbitmap("dice_icon.ico")
+        self.fontFamilyH1 = font.Font(family = 'Playfair Display', size = 40)
+        self.fontFamilyH2 = font.Font(family = 'Playfair Display', size = 25)
+        self.fontFamilyH3 = font.Font(family = 'Playfair Display', size = 15)
 
-        self.appLabel = ttk.Label(self, text = "Number Generator", font = (font_family, h1))        
+        self.appLabel = ttk.Label(self, text = "Number Generator", font = self.fontFamilyH1)        
 
         self.mainFrame = ttk.Frame(self)
 
@@ -26,24 +21,24 @@ class NumberGen(tk.Tk):
         self.inputFrame = ttk.Frame(self.mainFrame)
 
         self.startValue = tk.StringVar()
-        self.startEntry = ttk.Entry(self.inputFrame, textvariable = self.startValue, font = (font_family, h2), width = 5)
-        self.startLabel = ttk.Label(self.inputFrame, text = "Start value:", font = (font_family, h2))
+        self.startEntry = ttk.Entry(self.inputFrame, textvariable = self.startValue, width = 5, font = self.fontFamilyH2)
+        self.startLabel = ttk.Label(self.inputFrame, text = "Start value:", font = self.fontFamilyH2)
 
         self.endValue = tk.StringVar()
-        self.endEntry = ttk.Entry(self.inputFrame, textvariable = self.endValue, font = (font_family, h2), width = 5)
-        self.endLabel = ttk.Label(self.inputFrame, text = "End value:", font = (font_family, h2))
+        self.endEntry = ttk.Entry(self.inputFrame, textvariable = self.endValue, width = 5, font = self.fontFamilyH2)
+        self.endLabel = ttk.Label(self.inputFrame, text = "End value:", font = self.fontFamilyH2)
 
         # Generate button
-        self.generateButton = ttk.Button(self.mainFrame, text = "Generate")
+        self.generateButton = tk.Button(self, text = "Generate", font = self.fontFamilyH3)
 
         self.appLabel.pack(pady = "5")
         self.mainFrame.pack(pady = "5")
         self.inputFrame.grid()
         self.startLabel.grid(column = 0, row = 0)
-        self.startEntry.grid(column = 1, row = 0)
+        self.startEntry.grid(column = 1, row = 0, padx = 2)
         self.endLabel.grid(column = 0, row = 1)
         self.endEntry.grid(column = 1, row = 1)
-        self.generateButton.grid(pady = "10")
+        self.generateButton.pack(pady = "15")
 
 if __name__ == "__main__":
     root = NumberGen()
